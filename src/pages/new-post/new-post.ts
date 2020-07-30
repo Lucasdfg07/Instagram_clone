69c892530de6a6a0fd7b732f8dc2df1830e344ff
Lucas Siqueira Fernandes <lucassiqueirafernandes@MacBook-Air-de-Lucas.local>
2020-07-29 23:35:36 -0300

    Setting new post to api

--- src/pages/new-post/new-post.ts
+++ src/pages/new-post/new-post.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, ToastController, NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { AuthProvider } from '../../providers/auth/auth';
 import { UserProvider } from '../../providers/user/user';
 import { User } from '../../models/user';
-import { Post } from '../../models/post';
+import { PostProvider } from '../../providers/post/post';
+import { NgForm } from '@angular/forms';
  
 @IonicPage()
 @Component({
@@ -14,14 +15,20 @@ import { Post } from '../../models/post';
 export class NewPostPage {
  
   public user: User;
-  public post: Post;
+  public postPhoto = "";
+  
+  private base64Content = "";
   private cameraConfig: CameraOptions = {
-    quality: 100,
+    quality: 10,
+    destinationType: this.camera.DestinationType.DATA_URL,
     mediaType: this.camera.MediaType.PICTURE,
     correctOrientation: true
   }
  
-  constructor(private userProvider: UserProvider, 
+  constructor(private userProvider: UserProvider,
+              private postProvider: PostProvider,
+              private nav: NavController,
+              private toast: ToastController, 
               private auth: AuthProvider, 
               private camera: Camera) {
   }
@@ -32,7 +39,21 @@ export class NewPostPage {
  
  
   takePicture() {
-    this.camera.getPicture(this.cameraConfig).then(uri => console.log(uri), () => {})
+    this.camera.getPicture(this.cameraConfig).then(base64 => {
+      this.base64Content = base64;
+      this.postPhoto = "data:image/jpeg;base64," +base64;
+    }, () => {})
+  }
+ 
+ 
+  createPost(form: NgForm) {
+    this.postProvider.create(this.base64Content, form.value.description).then((response: any) => {
+      this.nav.parent.select(0);
+      this.toast.create({ message: "Post created successfully", duration: 3000 }).present();
+      this.postPhoto = "";
+    }, (response: any) => {
+      response.error.errors.forEach(error => this.toast.create({ message: error, duration: 2000 }).present())
+    })
   }
  
   
--- src/providers/post/post.ts
+++ src/providers/post/post.ts
@@ -29,5 +29,11 @@ export class PostProvider {
     const response: any = await this.http.get(`${API_URL}/api/v1/users/${userId}/posts`, { headers: this.auth.authHeader() }).toPromise();
     return this.formatPost(response);
   }
+
+  async create(photo, description) {
+    const post = { photo_base64: photo, description: description }
+    const response: any = await this.http.post(`${API_URL}/api/v1/posts`, { post: post}, { headers: this.auth.authHeader() }).toPromise();
+    return response;
+  }
  
 }
